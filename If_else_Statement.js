180e792b4badf6ca1d3e78088bd3cec6ab9dae12
AbinayaSelvam <sabinaya352@gmail.com>
2026-06-17 15:18:48 +0530

    If else statement

--- If_else_Statement.js
+++ If_else_Statement.js
@@ -32,3 +32,27 @@ else
 {
     console.log("Tail");
 }
+
+//4.arithmetic operations
+const prompt = require('prompt-sync')();
+
+let num1 = Number(prompt("Enter value of a: "));
+let num2 = Number(prompt("Enter value of b: "));
+let num3 = Number(prompt("Enter value of c: "));
+
+let result1 = num1 + num2 * num3;
+let result2 = num1 % num2 + num3;
+let result3 = num3 + num1 / num2;
+let result4 = num1 * num2 + num3;
+
+console.log("\nResults:");
+console.log("a + b * c =", result1);
+console.log("a % b + c =", result2);
+console.log("c + a / b =", result3);
+console.log("a * b + c =", result4);
+
+let max = Math.max(result1, result2, result3, result4);
+let min = Math.min(result1, result2, result3, result4);
+
+console.log("\nMaximum value =", max);
+console.log("Minimum value =", min);
